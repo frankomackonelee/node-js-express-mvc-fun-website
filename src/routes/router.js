4953fb2de76e1952703af539f89db46c424102c5
Francis Malone-Lee <francis@mathskitchen.com>
2024-01-26 21:05:15 +0000

    Adding a comedy story

--- src/routes/router.js
+++ src/routes/router.js
@@ -4,15 +4,16 @@ const router = Router()
 
 router.get('/', async (req, res) => res.render('index', {title: "My first website", p1: "This website uses ejs syntax to interpolate data into ejs files which create html pages"}))
 router.get('/about', async (req, res) => res.render('about', {title: "About"}))
-router.get('/contact', async (req, res) => res.render('contact', {title: "Contact"}))
+router.get('/create-story', async (req, res) => res.render('create-story', {title: "Create"}))
+router.get('/example', async (req, res) => res.render('story'))
 
 router.post('/submit-form', (req, res) => {
-    const { email, message } = req.body;
+    const body = req.body;
     
     // Process the data here (e.g., save to database, send an email, etc.)
     
     // Redirect or send a response after processing
-    res.send('Form submitted successfully!');
+    res.render('story')
 });
 
 export default router;
