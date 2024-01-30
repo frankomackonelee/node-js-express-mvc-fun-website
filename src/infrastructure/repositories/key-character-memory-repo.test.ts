673a69e88338a98815da8af20efca3599362d6e8
Francis Malone-Lee <francis@mathskitchen.com>
2024-01-30 19:54:43 +0000

    Set up unit tests

---
+++ src/infrastructure/repositories/key-character-memory-repo.test.ts
@@ -0,0 +1,14 @@
+
+
+describe('KeyCharacterMemoryRepository', () => {
+    it('should get a default character successfully', async () => {
+        // Arrange
+        // const _systemUnderTest = new KeyCharacterMemoryRepository();
+
+        // Act
+        // const response = await _systemUnderTest.getKeyCharacter(0);
+
+        // Assert
+        expect(true).toBe(true);
+    })
+});
